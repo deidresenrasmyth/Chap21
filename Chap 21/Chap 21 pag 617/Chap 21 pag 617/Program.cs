534a37454d458bd533d0152e2ca73f0500ab638b
Deidre <deidre.balard@outlook.com>
2020-03-09 19:08:58 -0400

    commit

---
+++ Chap 21/Chap 21 pag 617/Chap 21 pag 617/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Chap_21_pag_617
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileStream aFile = new FileStream("Long.txt", FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(aFile);
+                bool truth = true;
+                //Write data to file.
+                sw.WriteLine("Hello to you");
+                sw.WriteLine("It is now {0} and thing are looking good.",
+                    DateTime.Now.ToLongDateString());
+                sw.Write("More than that,");
+                sw.Write("it's {0} than C# is fun.", truth);
+                sw.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An IO exception has been thrown!");
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
+
+    
+
---
+++ Chap 21/Chap 21 pag 619/Chapt 21 pag619/Chapt 21 pag619/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Chapt_21_pag619
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            try
+            {
+                FileStream aFile = new FileStream("Long.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+                line = sr.ReadLine();
+                //read date in line by line.
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An IO exception has been throw!");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+            Console.ReadKey();
+        }
+        }
+    }
+
